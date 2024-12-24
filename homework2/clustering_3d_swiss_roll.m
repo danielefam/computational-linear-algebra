dfe8d9a47bda194dda9e6d17927f1c6c542da153
Alessandro Perlo <aleperlo002@gmail.com>
2024-12-24 12:36:58 +0100

    Add clustering on 3d data

---
+++ homework2/clustering_3d_swiss_roll.m
@@ -0,0 +1,17 @@
+X = synthetic_dataset(42);
+
+sigma = 1;
+k = 10;
+
+[L, D, W, L_sym] = compute_laplacian(X, sigma, k);
+eigs_computation = @(A) eigs(A, 20, 'smallestabs');
+plot_graphs(X, L, W, eigs_computation);
+
+[Bins, binsizes] = conncomp(graph(L));
+conncomp_graphs_algorithm = size(binsizes, 2);
+disp(['# Connected components (graph algorithm): ', num2str(conncomp_graphs_algorithm)]);
+conncomp_rank = size(X, 1) - rank(full(L));
+disp(['# Connected components (rank): ', num2str(conncomp_rank)]);
+
+M = 9;
+kmeans_plot(X, L, M);
