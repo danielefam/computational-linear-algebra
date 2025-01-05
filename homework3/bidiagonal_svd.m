53773ba4ea6c1c8308042ee096bdf0ee8d2743dc
Alessandro Perlo <aleperlo002@gmail.com>
2025-01-05 11:05:42 +0100

    Fixed matrix size for bidiagonal SVD

--- homework3/bidiagonal_svd.m
+++ homework3/bidiagonal_svd.m
@@ -1,17 +1,21 @@
-function [U2, S, V2] = bidiagonal_svd(B, maxit, tol)
+function [U2, S, V2] = bidiagonal_svd(B_full, k, maxit, tol)
 
 if nargin < 2
-    maxit = 1000;
+    k = size(B_full, 2);
 end
 if nargin < 3
+    maxit = 1000;
+end
+if nargin < 4
     tol = 1e-12;
 end
-
-n = size(B, 1);
+m = size(B_full, 1);
+n = size(B_full, 2);
+B = B_full(1:n, 1:n);
 a2 = diag(B).^2;
 b2 = diag(B, 1).^2;
 
-U2 = zeros(n);
+U2 = zeros(m);
 V2 = zeros(n);
 
 for i = 1:maxit
@@ -31,9 +35,9 @@ end
 S = diag(sqrt(a2));
 T = B'*B;
 eigs = zeros(n, 1);
-for i = 1:n
+for i = 1:k
     [eigs(i), V2(:,i)] = shifted_inverse_power_method(T, a2(i));
-    U2(:,i) = B*V2(:,i) / S(i,i);
+    U2(:,i) = B_full*V2(:,i) / S(i,i);
 end
 
 end
