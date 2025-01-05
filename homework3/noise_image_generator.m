7bf6493baddbd820dc98dd9a7b43cdd8991952a3
Daniele Famà <daniele260318@gmail.com>
2025-01-05 11:29:33 +0100

    Added noise image generatore and noise image

---
+++ homework3/noise_image_generator.m
@@ -0,0 +1,15 @@
+function [img] = noise_image_generator(row, col)
+    
+    if nargin < 2
+        col = 640;
+    end
+
+    if nargin < 1
+        row = 427;
+    end
+
+    img = randi([0, 255], row, col);
+    img = uint8(img);
+    imshow(img);
+    imwrite(img, 'data/noise_image.jpg');    
+end
