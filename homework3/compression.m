ad57a87e6336f95a9418c92fdd066c9d67ed7fb9
Daniele Famà <daniele260318@gmail.com>
2025-01-06 19:04:52 +0100

    Add cumulative explained variance visualization. Refactor compression function to improve singular value plotting

--- homework3/compression.m
+++ homework3/compression.m
@@ -1,7 +1,7 @@
 function compression(img, k, name)
     
-    channel_name_color = {'Red', 'Green', 'Blue'};
-    channel_color = {'r', 'g', 'b'};
+    channel_name_colors = {'Red', 'Green', 'Blue'};
+    channel_colors = {'r', 'g', 'b'};
     % Read and convert the image to greyscale
     if ndims(img) == 3
         greyscale_img = rgb2gray(img);
@@ -13,10 +13,14 @@ function compression(img, k, name)
 
     % Compress the greyscale image
     [U, S, V] = svd_custom(double(greyscale_img), k);
+    
+    
+    plot_singular_values(diag(S), ['Greyscale ', name, ' image singular values']);
+    plot_cumulative_explained_variance(diag(S), ['Greyscale ', name, ' image explained variance']);
 
-    figure;
-    plot_singular_values(S)
-    title(['Greyscale ', name, ' image singular values'])
+    % figure;
+    % plot_cumulative_explained_variance(S)
+    % title(['Greyscale ', name, ' image singular values'])
 
     img_compressed = U(:,1:k)*S(1:k,1:k)*V(:,1:k)';
 
@@ -32,24 +36,18 @@ function compression(img, k, name)
     
     show_image(img_color, ['Original ' , name, ' image'])
 
-    figure;
-    hold on;
     for channel = 1:3
-        color_name = channel_name_color{channel};
-        color = channel_color{channel};
+        
         img_channel = double(img_color(:,:,channel));
         [U, S, V] = svd_custom(img_channel, k);
-
-        image_title = ['Colorscale ', name, ' image singular values'];
-        
-        plot_singular_values(S, color_name, color)
-        title(image_title);        
+        singular_values(:, channel) = diag(S);       
 
         img_compressed_channel = U(:,1:k)*S(1:k,1:k)*V(:,1:k)';
         img_compressed_color(:,:,channel) = img_compressed_channel;
     end
-    hold off;
-    legend(Location='northeast');
    
+    plot_singular_values(singular_values, ['Colorscale ', name, ' image singular values'], channel_name_colors, channel_colors)
+    plot_cumulative_explained_variance(singular_values, ['Colorscale ', name, ' image explained variance'], channel_name_colors, channel_colors)
+
     show_image(uint8(img_compressed_color), ['Compressed ', name,' image with k = ', num2str(k)]);
 end
--- homework3/plot_cumulative_explained_variance.m
+++ homework3/plot_cumulative_explained_variance.m
@@ -1,5 +1,4 @@
-function [] = plot_singular_values(singular_values, img_title, colors_names, colors)
-
+function [] = plot_cumulative_explained_variance(singular_values, img_title, colors_names, colors)
     if nargin < 3
         colors_names = {'grey'};
     end
@@ -7,12 +6,15 @@ function [] = plot_singular_values(singular_values, img_title, colors_names, col
         colors = {'b'};
     end
 
-    if size(singular_values,2) == 1
+    explained_variance = cumsum(singular_values);
+    explained_variance = explained_variance / explained_variance(end);
+
+    if size(explained_variance,2) == 1
         color_name = colors_names{1};
         color = colors{1};
         figure;
         % set(gcf, 'DefaultAxesFontSize', 15, 'DefaultLegendFontSize', 12,  'Units', 'inches', 'PaperUnits', 'inches', 'PaperSize', [6, 4.5], 'Position', [0, 0, 6, 4], 'PaperPositionMode', 'auto');
-        plot(abs(singular_values), DisplayName=color_name, LineWidth=1, Color=color);
+        plot(abs(explained_variance), DisplayName=color_name, LineWidth=1, Color=color);
         xlabel('i');
         ylabel('|\sigma_i|');
         title(img_title);
@@ -25,7 +27,7 @@ function [] = plot_singular_values(singular_values, img_title, colors_names, col
     for j = 1:length(colors)    
         color_name = colors_names{j};
         color = colors{j};
-        plot(abs(singular_values(:,j)), DisplayName=color_name, LineWidth=1, Color=color);
+        plot(abs(explained_variance(:,j)), DisplayName=color_name, LineWidth=1, Color=color);
     end
     title(img_title);  
     xlabel('i');
@@ -33,6 +35,5 @@ function [] = plot_singular_values(singular_values, img_title, colors_names, col
     grid on;   
     hold off;
     legend(Location='northeast');
-
 end
 
