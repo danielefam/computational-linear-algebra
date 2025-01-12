1a5ab8b0f08b8432c33fb6e42a11a5ab08c39456
Daniele Famà <daniele260318@gmail.com>
2025-01-12 15:52:25 +0100

    Add comments to deflation_eigenvalues and inverse_power_method functions

--- homework2/inverse_power_method.m
+++ homework2/inverse_power_method.m
@@ -7,30 +7,40 @@ function [mu, v] = inverse_power_method(A, maxIter, relTol)
     end
 
     [n,~] = size(A);
-    v_in = sprand(n,1,0.2);
 
+    if n == 1
+        mu = A;
+        v = 1;
+        return;
+    end
+
+    % Initial gues of the eigenpair
+    v_in = sprand(n,1,0.05);
     v_k = v_in / norm(v_in);
     mu_k = 10; 
-    % A = full(A);
+    
+    % LU factorization of A to solve the linear system faster
     [L, U] = lu(A);
 
     for k = 1:maxIter
-        v_til_k1 = U \ (L \ v_k);
-        % v_til_k1 = A \ v_k;        
+        % Solve the linear system
+        v_til_k1 = U \ (L \ v_k);     
         
+        % Eigenpair update
         mu_k1 = v_k' * v_til_k1;
-
         v_k1 = v_til_k1 / norm(v_til_k1);
 
+        % Check convergence
         if abs(mu_k1 - mu_k) < relTol * abs(mu_k1)
             break;
         end
 
+        % Update the eigenpair
         v_k = v_k1;
-        mu_k = mu_k1;
-        
+        mu_k = mu_k1;        
     end
 
+    % Output the eigenpair
     mu = 1 / mu_k;
     v = v_k/norm(v_k);
 end
